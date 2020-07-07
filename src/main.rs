7e482e753a487532ed1bc474a13d2fbecffdca6a
Manuel Cerón <ceronman@gmail.com>
2020-07-07 09:36:27 +0200

    Simple guessing game to warmup rust.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::io;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+    let number = 46;
+
+    loop {
+        println!("Guess a number: ");
+
+        let mut buf = String::new();
+
+        io::stdin()
+            .read_line(&mut buf)
+            .expect("Error reading guess");
+
+        let guess: i32 = buf.trim().parse().expect("Please type a number");
+
+        match guess.cmp(&number) {
+            Ordering::Greater => println!("Greater"),
+            Ordering::Less => println!("Less"),
+            Ordering::Equal => {
+                println!("Got it");
+                break;
+            }
+        }
+    }
 }
