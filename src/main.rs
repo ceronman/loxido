5cf27c38c1860ac87fbdcf70ef19c1dc7ade7d45
Manuel Cerón <ceronman@gmail.com>
2020-07-16 10:19:46 +0200

    Added simple instructions and disassembler.
    Baby steps.

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,73 @@
-use std::io;
-use std::cmp::Ordering;
+use std::env;
 
-fn main() {
-    let number = 46;
+type Value = f64;
 
-    loop {
-        println!("Guess a number: ");
+enum Instruction {
+    Constant(u8),
+    Return,
+}
 
-        let mut buf = String::new();
+struct Chunk {
+    code: Vec<Instruction>,
+    constants: Vec<Value>,
+    lines: Vec<usize>
+}
 
-        io::stdin()
-            .read_line(&mut buf)
-            .expect("Error reading guess");
+impl Chunk {
+    fn new() -> Chunk {
+        Chunk {
+            code: Vec::new(),
+            constants: Vec::new(),
+            lines: Vec::new(),
+        }
+    }
 
-        let guess: i32 = buf.trim().parse().expect("Please type a number");
+    fn write(&mut self, instruction: Instruction, line: usize) {
+        self.code.push(instruction);
+        self.lines.push(line);
+    }
 
-        match guess.cmp(&number) {
-            Ordering::Greater => println!("Greater"),
-            Ordering::Less => println!("Less"),
-            Ordering::Equal => {
-                println!("Got it");
-                break;
+    fn add_constant(&mut self, value: Value) -> u8 {
+        self.constants.push(value);
+        (self.constants.len() - 1) as u8
+    }
+
+    fn disassemble(&self, name: &str) {
+        println!("== {} ==", name);
+        for (offset, instruction) in self.code.iter().enumerate() {
+            self.disassemble_instruction(instruction, offset);
+        }
+    }
+    
+    fn disassemble_instruction(&self, instruction: &Instruction, offset: usize) {
+        print!("{:04} ", offset);
+        let line = self.lines[offset];
+        if offset > 0 && line == self.lines[offset -1] {
+            print!("   | ");
+        } else {
+            print!("{:>4} ", line);
+        }
+        match instruction {
+            Instruction::Return => println!("OP_RETURN"),
+            Instruction::Constant(index) => {
+                let i = *index;
+                let i =i as usize;
+                let value = self.constants[i];
+                println!("{:<16} {:4} {}", "OP_CONSTANT", index, value);
             }
         }
     }
+}
+
+fn main() {
+    let args: Vec<String> = env::args()
+        .map(|s| format!("\"{}\"", s))
+        .collect();
+    println!("{}", args.join(", "));
+
+    let mut chunk = Chunk::new();
+    chunk.write(Instruction::Return, 123);
+    let index = chunk.add_constant(1.2);
+    chunk.write(Instruction::Constant(index), 124);
+    chunk.disassemble("test chunk");
 }
