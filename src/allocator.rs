5dec5119bf14fee9fe7b3f061e7bcf762b66ae40
Manuel Ceron <ceronman@gmail.com>
2021-04-25 16:39:31 +0200

    Moved trace implementations to the function module

--- src/allocator.rs
+++ src/allocator.rs
@@ -3,10 +3,7 @@ use std::{any::Any, collections::HashMap, fmt, hash};
 
 use fmt::Debug;
 
-use crate::{
-    chunk::{Instruction, Table, Value},
-    function::{Closure, LoxFunction, ObjUpvalue, Upvalue},
-};
+use crate::chunk::{Table, Value};
 
 pub trait Trace {
     fn format(&self, f: &mut fmt::Formatter, allocator: &Allocator) -> fmt::Result;
@@ -40,79 +37,6 @@ impl Trace for String {
         mem::size_of::<String>() + self.as_bytes().len()
     }
     fn trace(&self, _allocator: &mut Allocator) {}
-    fn as_any(&self) -> &dyn Any {
-        self
-    }
-    fn as_any_mut(&mut self) -> &mut dyn Any {
-        self
-    }
-}
-
-impl Trace for ObjUpvalue {
-    fn format(&self, f: &mut fmt::Formatter, _allocator: &Allocator) -> fmt::Result {
-        write!(f, "upvalue")
-    }
-    fn size(&self) -> usize {
-        mem::size_of::<ObjUpvalue>()
-    }
-    fn trace(&self, allocator: &mut Allocator) {
-        if let Some(obj) = self.closed {
-            allocator.mark_value(obj)
-        }
-    }
-    fn as_any(&self) -> &dyn Any {
-        self
-    }
-    fn as_any_mut(&mut self) -> &mut dyn Any {
-        self
-    }
-}
-
-impl Trace for LoxFunction {
-    fn format(&self, f: &mut fmt::Formatter, allocator: &Allocator) -> fmt::Result {
-        let name = allocator.deref(self.name);
-        if name.is_empty() {
-            write!(f, "<script>")
-        } else {
-            write!(f, "<fn {}>", name)
-        }
-    }
-    fn size(&self) -> usize {
-        mem::size_of::<LoxFunction>()
-            + self.upvalues.capacity() * mem::size_of::<Upvalue>()
-            + self.chunk.code.capacity() * mem::size_of::<Instruction>()
-            + self.chunk.constants.capacity() * mem::size_of::<Value>()
-            + self.chunk.constants.capacity() * mem::size_of::<usize>()
-    }
-    fn trace(&self, allocator: &mut Allocator) {
-        allocator.mark_object(self.name);
-        for &constant in &self.chunk.constants {
-            allocator.mark_value(constant);
-        }
-    }
-    fn as_any(&self) -> &dyn Any {
-        self
-    }
-    fn as_any_mut(&mut self) -> &mut dyn Any {
-        self
-    }
-}
-
-impl Trace for Closure {
-    fn format(&self, f: &mut fmt::Formatter, allocator: &Allocator) -> fmt::Result {
-        let function = allocator.deref(self.function);
-        function.format(f, allocator)
-    }
-    fn size(&self) -> usize {
-        mem::size_of::<Closure>()
-            + self.upvalues.capacity() * mem::size_of::<Reference<ObjUpvalue>>()
-    }
-    fn trace(&self, allocator: &mut Allocator) {
-        allocator.mark_object(self.function);
-        for &upvalue in &self.upvalues {
-            allocator.mark_object(upvalue);
-        }
-    }
     fn as_any(&self) -> &dyn Any {
         self
     }
--- src/function.rs
+++ src/function.rs
@@ -1,6 +1,11 @@
-use std::fmt;
+use std::{any::Any, fmt, mem};
 
-use crate::{allocator::Reference, chunk::Chunk, chunk::Value, vm::Vm};
+use crate::{
+    allocator::{Allocator, Reference, Trace},
+    chunk::Chunk,
+    chunk::{Instruction, Value},
+    vm::Vm,
+};
 
 #[derive(Clone, Copy)]
 pub struct NativeFn(pub fn(&Vm, &[Value]) -> Value);
@@ -31,6 +36,36 @@ pub struct LoxFunction {
     pub upvalues: Vec<Upvalue>,
 }
 
+impl Trace for LoxFunction {
+    fn format(&self, f: &mut fmt::Formatter, allocator: &Allocator) -> fmt::Result {
+        let name = allocator.deref(self.name);
+        if name.is_empty() {
+            write!(f, "<script>")
+        } else {
+            write!(f, "<fn {}>", name)
+        }
+    }
+    fn size(&self) -> usize {
+        mem::size_of::<LoxFunction>()
+            + self.upvalues.capacity() * mem::size_of::<Upvalue>()
+            + self.chunk.code.capacity() * mem::size_of::<Instruction>()
+            + self.chunk.constants.capacity() * mem::size_of::<Value>()
+            + self.chunk.constants.capacity() * mem::size_of::<usize>()
+    }
+    fn trace(&self, allocator: &mut Allocator) {
+        allocator.mark_object(self.name);
+        for &constant in &self.chunk.constants {
+            allocator.mark_value(constant);
+        }
+    }
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+    fn as_any_mut(&mut self) -> &mut dyn Any {
+        self
+    }
+}
+
 #[derive(Debug)]
 pub struct ObjUpvalue {
     pub location: usize, // TODO: Make this a proper type
@@ -46,6 +81,26 @@ impl ObjUpvalue {
     }
 }
 
+impl Trace for ObjUpvalue {
+    fn format(&self, f: &mut fmt::Formatter, _allocator: &Allocator) -> fmt::Result {
+        write!(f, "upvalue")
+    }
+    fn size(&self) -> usize {
+        mem::size_of::<ObjUpvalue>()
+    }
+    fn trace(&self, allocator: &mut Allocator) {
+        if let Some(obj) = self.closed {
+            allocator.mark_value(obj)
+        }
+    }
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+    fn as_any_mut(&mut self) -> &mut dyn Any {
+        self
+    }
+}
+
 #[derive(Debug)]
 pub struct Closure {
     pub function: Reference<LoxFunction>,
@@ -58,5 +113,28 @@ impl Closure {
             function,
             upvalues: Vec::new(),
         } // TODO: use .with_capacity
+    }
+}
+
+impl Trace for Closure {
+    fn format(&self, f: &mut fmt::Formatter, allocator: &Allocator) -> fmt::Result {
+        let function = allocator.deref(self.function);
+        function.format(f, allocator)
+    }
+    fn size(&self) -> usize {
+        mem::size_of::<Closure>()
+            + self.upvalues.capacity() * mem::size_of::<Reference<ObjUpvalue>>()
+    }
+    fn trace(&self, allocator: &mut Allocator) {
+        allocator.mark_object(self.function);
+        for &upvalue in &self.upvalues {
+            allocator.mark_object(upvalue);
+        }
+    }
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+    fn as_any_mut(&mut self) -> &mut dyn Any {
+        self
     }
 }
